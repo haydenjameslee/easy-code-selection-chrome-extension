0aa3b0947ac5399cb06d990aa413a30c395bfa0b
Hayden Lee <haydenjameslee@gmail.com>
2014-01-02 09:31:08 -0500

    init commit

---
+++ selectcode.js
@@ -0,0 +1,56 @@
+/**
+ * Creates the range of text to select and selects it
+ */
+function selectText(element) {
+    var range, selection;
+
+    if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+//On load event handlers
+jQuery(document).ready(function(){
+    var flag;
+    jQuery('code').on('mousedown', function() {
+        flag = 0;
+    });
+
+    jQuery('code').on('mousemove', function(){
+        flag = 1;
+    });
+
+    jQuery('code').on('mouseup', function(){
+        if(flag === 0){
+            selectText(jQuery(this)[0]);
+        }
+    });
+});
+
+//On load event handlers for jQuery 1.6.1
+jQuery(document).ready(function(){
+    var flag;
+    jQuery('code').live('mousedown', function() {
+        flag = 0;
+    });
+
+    jQuery('code').live('mousemove', function(){
+        flag = 1;
+    });
+
+    jQuery('code').live('mouseup', function(){
+        if(flag === 0){
+            selectText(jQuery(this)[0]);
+        }
+    });
+});
+
+
